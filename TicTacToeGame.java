200c3703021f08ebb6dc2529be1154e8ce575c94
balu-pinisety <manikantapandu225@gmail.com>
2021-06-20 22:13:55 +0530

    Added: Method for User to choose mark

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -8,10 +8,13 @@ public class TicTacToeGame {
 	
 	//variables
 	private static char[] element;
+	static char userMark, computerMark;
 	
 	public static void main(String[] args) {
 		//for creating empty elements
 		boardCreation();
+		//for user to choose 'X' or 'O' mark
+		choosingXorO();
 	}
 	
 	/**
@@ -23,6 +26,27 @@ public class TicTacToeGame {
 		element=new char[10];
 		for(int i=1; i < 10; i++) {
 			element[i]=' ';
+		}
+	}
+	
+	/**
+	 * Asking user to choose X or O
+	 * @param option
+	 * @return userMark, computerMark
+	 */
+	private static void choosingXorO() {
+		System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
+		int option = Utility.getUserInteger();
+		switch (option) {
+		case 1: userMark = 'X';
+				computerMark = 'O';
+						break;
+		case 2: userMark = 'O';
+			computerMark = 'X';
+			break;
+		default:
+			System.out.println("Your input is invalid");
+			choosingXorO();
 		}
 	}
 }
